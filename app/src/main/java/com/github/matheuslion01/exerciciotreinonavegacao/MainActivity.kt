def1f34522fc3b51a4e6322487f30042ace2e98c
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-17 20:01:03 -0300

    Adiciona chamada da LoginScreen na MainActivity como tela inicial do app

--- app/src/main/java/com/github/matheuslion01/exerciciotreinonavegacao/MainActivity.kt
+++ app/src/main/java/com/github/matheuslion01/exerciciotreinonavegacao/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.github.matheuslion01.exerciciotreinonavegacao.screens.LoginScreen
 import com.github.matheuslion01.exerciciotreinonavegacao.ui.theme.ExercicioTreinoNavegacaoTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,6 +21,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             ExercicioTreinoNavegacaoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                    LoginScreen(modifier = Modifier.padding(innerPadding))
+
 
                 }
             }
